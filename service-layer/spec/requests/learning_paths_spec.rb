0886b124a14a363fc38f5bc6f106032be98c7503
Alex Bradley <alex.robert.bradley@gmail.com>
2018-11-06 10:37:29 -0500

    Set up VCR for testing.
    The tests do not run yet, but I set up VCR to record api calls and play
    them back for testing.

--- service-layer/spec/requests/learning_paths_spec.rb
+++ service-layer/spec/requests/learning_paths_spec.rb
@@ -3,9 +3,12 @@ require "rails_helper"
 describe "LearningPaths" do
   describe "GET /learning_paths" do
     it "gets a list of all courses" do
-      get '/learning_paths'
+      # stub_request(:get, ENV["CANVAS_HOST"] + "/api/v1/courses").to_return(status: 200, body: "")
+      # stub_request(:get, ENV["CANVAS_HOST"] + "/api/v1/courses").to_rack(FakeCanvas)
+      get "/learning_paths"
 
       expect(response).to be_successful
+
     end
   end
 end
