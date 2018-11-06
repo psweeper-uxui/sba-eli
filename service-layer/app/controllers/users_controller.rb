a7f904f1e603e34598ea52466b69048da61232e3
Kelly <kdyer@users.noreply.github.com>
2018-11-06 16:01:29 -0500

    Update user object to align with the rest of the APIs

--- service-layer/app/controllers/users_controller.rb
+++ service-layer/app/controllers/users_controller.rb
@@ -1,131 +1,101 @@
 class UsersController < ApplicationController
-  include Response
-
-  #TODO: abstract out the http and authorization calls
-
-  def account_id
-    ENV["CANVAS_ACCOUNT_ID"]
-  end
-
-  def user_id
-    #TODO: grab this value being passed in as a parameter
-    "10" #self.find(params[:id])
-  end
-
-  def canvas_namespace
-    "sba-eli-service"
-  end
-
-  def canvas_accounts_url
-    ENV["CANVAS_HOST"] + "/api/v1/accounts/" + self.account_id + "/users/"
-  end
-
-  def canvas_user_url
-    ENV["CANVAS_HOST"] + "/api/v1/users/" + self.user_id
-  end
-
-  def canvas_custom_data_url
-    ENV["CANVAS_HOST"] + "/api/v1/users/" + self.user_id + "/custom_data"
-  end
-
 
   def index
-    response = HTTParty.get(self.canvas_accounts_url,
-                            :headers => {"Authorization" => "Bearer " + session["omniauth.auth"]["credentials"].token})
-    render json: response.body, status: 200
+    @user = User.new(session_token).all
+    render json: @user, status: 200
   end
 
-  def read_user
-    response = HTTParty.get(self.canvas_user_url,
-                            :headers => {"Authorization" => "Bearer " + session["omniauth.auth"]["credentials"].token})
-    render json: response.body, status: 200
+  def show
+    @user = User.new(session_token).read_user(params[:id])
+    render json: @user, status: 200
   end
 
-  def read_custom_data
-    response = HTTParty.get(self.canvas_custom_data_url + "?ns=" + self.canvas_namespace,
-                            :headers => {"Authorization" => "Bearer " + session["omniauth.auth"]["credentials"].token})
-    render json: response.body, status: 200
+  def show_custom
+    @user = User.new(session_token).read_user_custom_data(params[:id])
+    render json: @user, status: 200
   end
 
-  def create_user
-    response = HTTParty.post(self.canvas_accounts_url,
-                             :headers => {"Authorization" => "Bearer " + session["omniauth.auth"]["credentials"].token},
-                             :body => user_body_request("User Test",
-                                                        "Test, User",
-                                                        "U-Test",
-                                                        nil,
-                                                        "email+1@gmail.com",
-                                                        "unique_id+1@gmail.com"))
-
-    render json: response.body, status: 200
+  def create
+    @user = User.new(session_token).create_user(user_body_request)
+    render json: @user
   end
 
-  def update_user
-    response = HTTParty.put(self.canvas_user_url,
-                            :headers => {"Authorization" => "Bearer " + session["omniauth.auth"]["credentials"].token},
-                            :body => user_body_update_request("User Test",
-                                                              "Test, User",
-                                                              "U-Test",
-                                                              nil,
-                                                              "email+2@gmail.com",
-                                                              "https://picsum.photos/200"))
-
-    render json: response.body, status: 200
+  def update
+    @user = User.new(session_token).update_user(params[:id], user_body_update_request)
+    render json: @user
   end
 
-  def user_custom_data(json_data)
-    #example data: {'fruit': 'kiwi', 'candy': 'baby ruth'}
-    body = {
-        'ns': self.canvas_namespace,
-        'data': json_data
-    }
-    response = HTTParty.put(self.canvas_custom_data_url,
-                            :headers => {"Authorization" => "Bearer " + session["omniauth.auth"]["credentials"].token},
-                            :body => body)
-
-    render json: response.body, status: 200
+  def custom
+    @user = User.new(session_token).user_custom_data(params[:id], custom_data_request)
+    render json: @user
   end
 
-  def delete_user
-    response = HTTParty.delete(self.canvas_accounts_url + self.user_id,
-                               :headers => {"Authorization" => "Bearer " + session["omniauth.auth"]["credentials"].token})
-
-    render json: response.body, status: 200
+  def destroy
+    User.new(session_token).destroy_custom_data(params[:id])
+    User.new(session_token).destroy(params[:id])
   end
 
-  def delete_custom_data
-    response = HTTParty.delete(self.canvas_custom_data_url,
-                               :headers => {"Authorization" => "Bearer " + session["omniauth.auth"]["credentials"].token})
-    render json: response.body, status: 200
+  private
+
+  # TODO: revisit when we're using different auth
+  def session_token
+    ENV["CANVAS_TOKEN"]
   end
 
   #TODO: only send fields that have values
-  def user_body_request(name, sortable_name, short_name, birth_date, email, unique_id)
+  def user_body_request
     {
         'user' => {
-            'name': name,
-            'sortable_name': sortable_name,
-            'short_name': short_name,
-            'birthdate': birth_date,
-            'email': email
+            'name': "Test User",
+            'sortable_name': "User, Test",
+            'short_name': "Test",
+            'birthdate': nil,
+            'email': "email+1234@gmail.com"
         },
         'pseudonym': {
-            'unique_id': unique_id
+            'unique_id': "email+1234@gmail.com"
         }
     }
   end
 
   #TODO: consolidate with above request
-  def user_body_update_request(name, sortable_name, short_name, birth_date, email, avatar)
+  def user_body_update_request
     {
         'user' => {
-            'name': name,
-            'sortable_name': sortable_name,
-            'short_name': short_name,
-            'birthdate': birth_date,
-            'email': email,
+            'name': "Test D User",
+            'sortable_name': "User, Test D",
+            'short_name': "T",
+            'birthdate': nil,
+            'email': "email+1234@gmail.com",
             'avatar': {
-                'url': avatar
+                'url': "https://picsum.photos/200"
+            }
+        }
+    }
+  end
+
+  def custom_data_request
+    {
+        'ns': ENV['CANVAS_NAMESPACE'],
+        'data' => {
+            'user' => {
+                'linked_in': "http://www.google.com",
+                'zipcode': "06320",
+                'percent_ownership': 97,
+                'goal_percent_revenue': 12,
+                'goal_revenue_amount': 1000,
+                'goal_percent_increase_employees': 15,
+                'goal_increase_employee_amount': 160,
+                'race': ["race1", "race2"],
+                'ethnicity': "yes"
+            },
+            'company' => {
+                'company_id': "12312",
+                'company_name': "New Company",
+                'industry': ["one", "two", "three"],
+                'naics': ["1234", "123123", "8976"],
+                'num_employees': 40,
+                'website': "http://www.website.com"
             }
         }
     }
