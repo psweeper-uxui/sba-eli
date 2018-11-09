c0152bb772a5127ae9e4dab01f4a32d22f5ac180
Kelly <kdyer@users.noreply.github.com>
2018-11-09 15:17:14 -0500

    Why: Testing the user update functionality This change addresses the need by: Modified update test for the user object so that it does not get a JSON parse error

--- service-layer/spec/requests/users_spec.rb
+++ service-layer/spec/requests/users_spec.rb
@@ -34,45 +34,9 @@ describe UsersController do
       VCR.use_cassette("users/update_user") do
         user_id = 16
 
-        params = {
-        "user_data": {
-            "user": {
-                "name": "foo",
-                "sortable_name": "S, Kelly",
-                "short_name": "Kelly",
-                "email": "email+3446@gmail.com"
-            },
-            "pseudonym": {
-                "unique_id": "email+789@gmail.com"
-            }
-        },
-        "custom_data": {
-            "ns": ENV["CANVAS_NAMESPACE"],
-            "data": {
-                "user": {
-                    "linked_in": "http://www.google.com",
-                    "zipcode": "06320",
-                    "percent_ownership": 97,
-                    "goal_percent_revenue": 12,
-                    "goal_revenue_amount": 1000,
-                    "goal_percent_increase_employees": 15,
-                    "goal_increase_employee_amount": 160,
-                    "race": ["race1", "race2"],
-                    "ethnicity": "yes"
-                },
-                "company": {
-                    "company_id": "12312",
-                    "company_name": "New Company",
-                    "industry": ["one", "two", "three"],
-                    "naics": ["1234", "123123", "8976"],
-                    "num_employees": 40,
-                    "website": "http://www.website.com"
-                }
-            }
-        }
-    }
+        params = "{	\"user_data\": {\"user\":{\"name\":\"foo\",\"sortable_name\":\"S,Kelly\",\"short_name\":\"Kelly\",\"email\":\"email+3446@gmail.com\"},\"pseudonym\":{\"unique_id\":\"email+789@gmail.com\"}},\"custom_data\":{\"data\":{\"user\":{\"linked_in\":\"http://www.google.com\",\"zipcode\":\"06320\",\"percent_ownership\":97,\"goal_percent_revenue\":12,\"goal_revenue_amount\":1000,\"goal_percent_increase_employees\":15,\"goal_increase_employee_amount\":160,\"race\":[\"race1\",\"race2\"],\"ethnicity\":\"yes\"},\"company\":{\"company_id\":\"12312\",\"company_name\":\"NewCompany\",\"industry\":[\"one\",\"two\",\"three\"],\"naics\":[\"1234\",\"123123\",\"8976\"],\"num_employees\":40,\"website\":\"http://www.website.com\"}}}}"
 
-        header "HTTP_ACCEPT", "application/json"
+        headers "CONTENT_TYPE", "application/json"
         put "/users/#{user_id}", params: params
 
         json = JSON.parse(response.body)
