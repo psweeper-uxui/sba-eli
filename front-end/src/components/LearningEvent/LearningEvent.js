0478a811d437f12d1f8c94ad304b9f7825f5223d
Matthew Castle <matthew.ray.castle@gmail.com>
2018-11-15 08:00:34 -0800

    113 saving work in progress

---
+++ front-end/src/components/LearningEvent/LearningEvent.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+export default class LearningEvent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      learningEvent: {}
+    };
+  }
+
+  //TODO hook into service layer for event objects
+  async componentDidMount() {
+    const learningEvent = await this.learningEventObject();
+    this.setState({ learningEvent });
+  }
+
+  async learningEventObject() {
+    const res = await fetch(
+      "http://localhost:3000/learning_events/" +
+        this.props.match.params.id +
+        "?course_id=1"
+    );
+
+    return await res.json();
+  }
+  //How to render the objects we get back?
+  render() {
+    return <div>Learning Event</div>;
+  }
+}
