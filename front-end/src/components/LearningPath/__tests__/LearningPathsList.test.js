869401f91dc81236de14d02fa89387c13e1838a0
Andrew Wagner <awagner@Andrews-MacBook-Pro.local>
2018-11-16 13:05:51 -0500

    187 - fix broken tests

--- front-end/src/components/LearningPath/__tests__/LearningPathsList.test.js
+++ front-end/src/components/LearningPath/__tests__/LearningPathsList.test.js
@@ -15,23 +15,27 @@ describe('LearningPathsList', () => {
     expect(wrapper.find('div').length).toEqual(1);
   });
 
-  it('should render a <ul>', () => {
-    const wrapper = shallow(<LearningPathsList/>);
+  describe('rendering the list', () => {
+    var wrapper;
+    
+    beforeEach(function() {
+      const lps = [
+        { id: 1, name: "Course 1" },
+        { id: 2, name: "Course 2" },
+        { id: 3, name: "Course 3" },
+        { id: 4, name: "Course 4" },
+      ]
 
-    expect(wrapper.find('ul').length).toEqual(1);
-  });
-  
-  it('should render a 4 <li>', () => {
-    const lps = [
-      { id: 1, name: "Course 1" },
-      { id: 2, name: "Course 2" },
-      { id: 3, name: "Course 3" },
-      { id: 4, name: "Course 4" },
-    ]
+      wrapper = mount(<LearningPathsList/>);
+      wrapper.setState({learningPaths: lps})
+    })  
 
-    const wrapper = mount(<LearningPathsList/>);
-    wrapper.setState({learningPaths: lps})
-
-    expect(wrapper.find('li').length).toEqual(4);
-  });
+    it('should render a <ul>', () => {
+      expect(wrapper.find('ul').length).toEqual(1);
+    });
+    
+    it('should render a 4 <li>', () => {
+      expect(wrapper.find('li').length).toEqual(4);
+    });
+  })
 });
