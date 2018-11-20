d12b5c550373c14eb86ed638a196bfe345664e85
Matthew Castle <matthew.ray.castle@gmail.com>
2018-11-20 08:15:10 -0800

    Work in progress for learning Objectives refactor

--- front-end/src/components/LearningObjective/LearningObjective.js
+++ front-end/src/components/LearningObjective/LearningObjective.js
@@ -1,40 +1,54 @@
-import React from 'react';
-import { Header } from 'semantic-ui-react';
+import React, { Component } from "react";
+import axios from "axios";
+import { Header } from "semantic-ui-react";
+import LearningEventsList from "../LearningEvent/LearningEventsList";
 
-class LearningObjective extends React.Component {
+export default class LearningObjective extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       learningObjective: {}
-    }
+    };
   }
 
-  async componentDidMount() {
-    const learningObjective = await this.learningObjectiveObject();
-    this.setState({ learningObjective });
+  componentDidMount() {
+    this.learningObjectiveObject();
   }
 
   async learningObjectiveObject() {
-    const res = await fetch(
-      "http://localhost:3000/learning_objectives/" +
-      this.props.match.params.id +
-      "?course_id=1"
-    )
+    const objective_id = this.props.match.params.id;
+    const url =
+      process.env.REACT_APP_SERVICE_HOST + `/learning_events/${objective_id}`;
 
-    return await res.json();
+    const eventParams = {
+      course_id: this.props.match.params.course_id
+    };
+
+    axios
+      .get(url, { params: eventParams })
+      .then(res => {
+        const learningObjective = res.data;
+        this.setState({ learningObjective });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   render() {
     const learningObjective = this.state.learningObjective;
 
-    return(
+    return (
       <div>
-        <Header as='h1'>Learning Objective {learningObjective.id}</Header>
-        <Header as='h3'>{learningObjective.name}</Header>
+        <Header as="h1">Learning Objective {learningObjective.id}</Header>
+        <Header as="h3">{learningObjective.name}</Header>
+        <Header as="h4">Micro Learning Events:</Header>
+        <LearningEventsList
+          course_id={learningObjective.id}
+          module_id={learningObjective.module_id}
+        />
       </div>
-    )
+    );
   }
 }
-
-export default LearningObjective;
--- front-end/src/components/LearningObjective/LearningObjectives.js
+++ front-end/src/components/LearningObjective/LearningObjectives.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import LearningObjectivesList from './LearningObjectivesList';
+import React from "react";
+import LearningObjectivesList from "./LearningObjectivesList";
 
-class LearningObjectives extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {        
-    return(
+export default class LearningObjectives extends React.Component {
+  render() {
+    return (
       <div>
         <LearningObjectivesList course_id={this.props.course_id} />
       </div>
-    )
+    );
   }
 }
-
-export default LearningObjectives;
