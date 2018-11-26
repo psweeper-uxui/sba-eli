95e861ed29cb67916a2cedb79dc39cc02489c749
Matthew Castle <matthew.ray.castle@gmail.com>
2018-11-26 12:33:41 -0800

    fixed variables to be local variables not instance

--- service-layer/app/models/learning_event.rb
+++ service-layer/app/models/learning_event.rb
@@ -16,10 +16,10 @@ class LearningEvent
   end
 
   def find(course_id, module_id, id)
-    @url = "/courses/#{course_id}/modules/#{module_id}/items/#{id}"
-    @learning_event = self.class.get(@url, @options)
-    @learning_event["eventContent"] = get_content(@learningEvent["url"])
-    @learning_event
+    url = "/courses/#{course_id}/modules/#{module_id}/items/#{id}"
+    learning_event = self.class.get(url, @options)
+    learning_event["eventContent"] = get_content(learning_event["url"])
+    learning_event
   end
 
   def update(course_id, module_id, id, le_params)
