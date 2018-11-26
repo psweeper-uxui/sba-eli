d13e003aa1b07846dacbc5e743e282babd83b38c
Andrew Wagner <awagner@Andrews-MBP.fios-router.home>
2018-11-26 09:20:59 -0500

    SBAELIDEV-209 Sign up form tests
        Why:
    
        To ensure that the singup form continues to function
    
        This change addresses the need by:
    
        * Moving the SignUpForm to an appropriate place.
        * Adding a couple of tests for the SignUpForm

--- front-end/src/components/SignUpForm/SignUpForm.js
+++ front-end/src/components/SignUpForm/SignUpForm.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import axios from 'axios';
-import './App.css';
 import {Input, Container} from 'semantic-ui-react';
 var AmazonCognitoIdentity = require('amazon-cognito-identity-js');
 
---
+++ front-end/src/components/SignUpForm/__tests__/SignUpForm.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { form, List } from "semantic-ui-react";
+import SignUpForm from "../SignUpForm";
+import { shallow, mount } from "enzyme";
+
+describe("SignUpForm", () => {
+  it("should render correctly", () => {
+    const wrapper = shallow(<SignUpForm />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should render a <form>", () => {
+    const wrapper = shallow(<SignUpForm />);
+
+    expect(wrapper.find("form").length).toEqual(1);
+  });
+})
