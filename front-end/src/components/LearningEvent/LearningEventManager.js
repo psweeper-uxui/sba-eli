42ba77ecc66ab828d6741e3aac929d218c04c0da
Matthew Castle <matthew.ray.castle@gmail.com>
2018-11-26 10:30:52 -0800

    123 Infographic support to show learning event pages
    Learning events are different types of objects. I modified the
    learning event component to send the event to a manager class to
    then call the appropriate component depending on the type of
    event. In this case I was building functionality for Page type,
    which would match for infographics stored in canvas.

---
+++ front-end/src/components/LearningEvent/LearningEventManager.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import LearningEventInfoCard from "./LearningEventInfoCard";
+import LearningEventPage from "./LearningEventPage";
+
+export default class LearningEventManager extends Component {
+  renderEventContentByType() {
+    const event = this.props.event;
+    console.log("learning event", event);
+
+    if (event.type === "Page") {
+      return <LearningEventPage event={event} />;
+    } else {
+      return <LearningEventInfoCard event={event} />;
+    }
+  }
+  render() {
+    return this.renderEventContentByType();
+  }
+}
