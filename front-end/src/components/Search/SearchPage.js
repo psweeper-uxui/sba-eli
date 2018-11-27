f9ded7b0ddcb55c248b5c098e6a8342b94ce319f
Kelly <kdyer@users.noreply.github.com>
2018-11-27 15:42:26 -0500

    Leverage the url query parameter for the search term

--- front-end/src/components/Search/SearchPage.js
+++ front-end/src/components/Search/SearchPage.js
@@ -1,15 +1,23 @@
 import React, {Component} from "react";
 import "../../App.css";
 import {Grid} from "semantic-ui-react";
+import queryString from 'query-string';
 import SearchFacets from "./SearchFacets";
 import SearchResults from "./SearchResults";
 
 export default class SearchPage extends Component {
   render() {
+    let params = queryString.parse(this.props.location.search);
+    //TODO: does this need to be sanitized?
+    let searchTerm = params.searchTerm
+
     return (
         <Grid>
           <Grid.Row>
-            <SearchFacets/>
+            <h3>Search Results for '{searchTerm}'</h3>
+          </Grid.Row>
+          <Grid.Row>
+            <SearchFacets searchTerm={searchTerm} />
           </Grid.Row>
           <Grid.Row>
             <SearchResults/>
