a33f3fde410e4cc799575b4967e1cb7c80947ca7
Matthew Castle <matthew.ray.castle@gmail.com>
2018-11-29 09:42:14 -0800

    208 Time filters added for search
    Expanded on Kelly's code to add in the time filters to front end
    code in the same manner as previous work.

--- front-end/src/components/Search/SearchFacets.js
+++ front-end/src/components/Search/SearchFacets.js
@@ -1,24 +1,32 @@
-import React, {Component} from 'react'
-import {Form, Input} from 'semantic-ui-react'
-import MediaTypeFilters from "./MediaTypeFilters"
+import React, { Component } from "react";
+import { Form, Input } from "semantic-ui-react";
+import MediaTypeFilters from "./MediaTypeFilters";
+import TimeFilters from "./TimeFilters";
 
 export default class SearchFacets extends Component {
-
   render() {
     return (
-        <Form method='GET' action='/search'>
-          <Form.Group widths='equal'>
-            <Form.Field>
-              <label>Media Type</label>
-              <MediaTypeFilters/>
-            </Form.Field>
-          </Form.Group>
-          <Form.Group>
-            <Form.Button fluid>Cancel</Form.Button>
-            <Form.Button fluid type='submit'>Apply</Form.Button>
-          </Form.Group>
-          <Input type='hidden' name='searchTerm' value={this.props.searchTerm} />
-        </Form>
-    )
+      <Form method="GET" action="/search">
+        <Form.Group widths="equal">
+          <Form.Field>
+            <label>Media Type</label>
+            <MediaTypeFilters />
+          </Form.Field>
+        </Form.Group>
+        <Form.Group widths="equal">
+          <Form.Field>
+            <label>Time</label>
+            <TimeFilters />
+          </Form.Field>
+        </Form.Group>
+        <Form.Group>
+          <Form.Button fluid>Cancel</Form.Button>
+          <Form.Button fluid type="submit">
+            Apply
+          </Form.Button>
+        </Form.Group>
+        <Input type="hidden" name="searchTerm" value={this.props.searchTerm} />
+      </Form>
+    );
   }
 }
