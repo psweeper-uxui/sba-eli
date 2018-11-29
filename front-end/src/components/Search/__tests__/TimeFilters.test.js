16bbde18107bc3ce7da9b34c0d0c78a9492d726a
Matthew Castle <matthew.ray.castle@gmail.com>
2018-11-29 13:23:43 -0800

    208 Added tests to time filters

---
+++ front-end/src/components/Search/__tests__/TimeFilters.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { Form } from "semantic-ui-react";
+import TimeFilters from "../TimeFilters";
+
+describe("TimeFilters", () => {
+  it("should render 3 <Checkboxes> for 3 time lengths", () => {
+    const wrapper = shallow(<TimeFilters />);
+
+    expect(wrapper.find(Form.Checkbox).length).toEqual(3);
+  });
+
+  it("should render the checkbox with the expected data from the filter", () => {
+    const wrapper = shallow(<TimeFilters />);
+
+    expect(wrapper.find(Form.Checkbox).get(0).key).toEqual("short");
+    expect(wrapper.find(Form.Checkbox).get(0).props.value).toEqual("short");
+    expect(wrapper.find(Form.Checkbox).get(0).props.id).toEqual("short");
+    expect(wrapper.find(Form.Checkbox).get(0).props.label.children).toEqual(
+      "Less than 1 minute"
+    );
+  });
+});
