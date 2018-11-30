3edcd32711c2954b025773809bef2e02ff94756c
Kelly <kdyer@users.noreply.github.com>
2018-11-30 09:59:37 -0500

    Remove NavigationMenu from the project since NavBar is the current way to render the code

--- front-end/src/components/Navigation/__tests__/Navigation.test.js
+++ front-end/src/components/Navigation/__tests__/Navigation.test.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Navigation from '../Navigation';
-import NavigationMenu from '../NavigationMenu';
 import { shallow, mount } from 'enzyme';
 
 describe('Navigation', () => {
@@ -15,11 +13,5 @@ describe('Navigation', () => {
     const wrapper = shallow(<Navigation/>);
 
     expect(wrapper.find('div').length).toEqual(2);
-  });
-
-  it('should render a <NavigationMenu>', () => {
-    const wrapper = shallow(<Navigation/>);
-
-    expect(wrapper.find(NavigationMenu).exists()).toBe(true);
   });
 });
