cdc80bfd92f2c0fca2c45bd4be1c70def6eed0bf
Matthew Castle <matthew.ray.castle@gmail.com>
2018-11-30 11:25:46 -0800

    194 Add call to action to dashboard
    Added a call to action section to the dashboard section of the site
    this has a button that needs implementation for the about modal
    functionality.

---
+++ front-end/src/components/Dashboard/DashboardSplash.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { Grid, Header, Button } from "semantic-ui-react";
+
+export default class DashboardSplash extends Component {
+  render() {
+    return (
+      <Grid centered style={{ backgroundColor: "#2d78c9" }}>
+        <Grid.Row>
+          <Grid.Column mobile={16} tablet={8} computer={8} textAlign="center">
+            <Header as="h1" style={{ color: "#ffffff" }}>
+              Awesome Hero Caption
+            </Header>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row style={{ fontWeight: "bold" }}>
+          <Grid.Column mobile={16} tablet={8} computer={8} textAlign="center">
+            <div>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              commodo vel est ac tempor. Proin ultricies dictum risus, et tempus
+              felis blandit eget. Curabitur ac neque porttitor urna aliquet
+              vestibulum nec sit amet enim. Curabitur augue enim, vulputate
+              faucibus semper sed, elementum eu arcu. Maecenas id diam sem.
+              Nulla dictum eros vitae porta lobortis. Nunc iaculis ullamcorper
+              odio. Quisque sed rutrum diam, eget maximus nulla. Nullam sed
+              dolor quam. Fusce tempus quis ligula at pharetra. Nam eget erat et
+              justo tincidunt laoreet. In finibus massa magna, at pharetra nibh
+              aliquam id. Duis aliquet ex sit amet nulla mattis, consequat
+              mattis augue porta.
+            </div>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Button size="huge">Begin Your Journey</Button>
+        </Grid.Row>
+      </Grid>
+    );
+  }
+}
