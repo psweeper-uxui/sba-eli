ae56c3722e7eb8b4c0141e5f66b6065ac6a1b71a
Matthew Castle <matthew.ray.castle@gmail.com>
2018-11-30 12:21:15 -0800

    Added and fixed tests
    Added tests for the dashboard splash component. Moved the Dashboard
    component tests into a sub folder and fixed those, and fixed error
    with navigation test

--- front-end/src/components/Navigation/__tests__/Navigation.test.js
+++ front-end/src/components/Navigation/__tests__/Navigation.test.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import Navigation from '../Navigation';
-import { shallow, mount } from 'enzyme';
+import React from "react";
+import ReactDOM from "react-dom";
+import Navigation from "../Navigation";
+import Navbar from "../Navbar";
+import { shallow, mount } from "enzyme";
 
-describe('Navigation', () => {
-  it('should render correctly', () => {
-    const wrapper = shallow(<Navigation/>);
-    
+describe("Navigation", () => {
+  it("should render correctly", () => {
+    const wrapper = shallow(<Navigation />);
+
     expect(wrapper).toMatchSnapshot();
   });
-  
-  it('should render a <div>', () => {
-    const wrapper = shallow(<Navigation/>);
 
-    expect(wrapper.find('div').length).toEqual(2);
+  it("should render a <div>", () => {
+    const wrapper = shallow(<Navigation />);
+
+    expect(wrapper.find("div").length).toEqual(2);
+  });
+
+  it("should render a <Navbar>", () => {
+    const wrapper = shallow(<Navigation />);
+
+    expect(wrapper.find(Navbar).exists()).toBe(true);
   });
 });
