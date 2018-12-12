a55ebc3ab2c18ed64d17bb190ece9a65f7e7f3b5
Nicholas Watson <nicholas.w.watson@me.com>
2018-12-11 23:01:21 -0500

    Created contentable routing concern, added it to learning_paths and learning_objectives

--- service-layer/config/routes.rb
+++ service-layer/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  concern :contentable do
+    resource :custom_content
+  end
+
   resource :cofirmation_code, only: :create
   resources :learning_events
-  resources :learning_paths
-  resources :learning_objectives
+  resources :learning_paths, concerns: :contentable
+  resources :learning_objectives, concerns: :contentable
   resource :session, only: %I[create destroy]
   resource :sign_up, only: :create
   resources :users
