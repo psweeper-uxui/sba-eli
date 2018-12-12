a3d8c3937ae89e170b6f273c0e4bb8f038c0cd9e
tjfamodu <tjfamodu@gmail.com>
2018-12-12 04:00:06 -0500

    Fixing merge conflict

--- service-layer/docker-entrypoint.sh
+++ service-layer/docker-entrypoint.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 set -e
-<<<<<<< HEAD
  if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
  exec bundle exec "$@"
-=======
-
-if [ -f tmp/pids/server.pid ]; then
-  rm tmp/pids/server.pid
-fi
-
-exec bundle exec "$@"
->>>>>>> dockerized-service-frontend
