b1eb0a230e3591db0f02d621b3efbb43eccf24b4
Nicholas Watson <nicholas.w.watson@me.com>
2018-12-12 05:47:36 -0500

    updated content_search to return custom content

--- service-layer/app/models/queries/content_search.rb
+++ service-layer/app/models/queries/content_search.rb
@@ -29,18 +29,21 @@ module Queries
       <<-SQL
         -- Search learning paths
         SELECT DISTINCT
-          courses.id,
-          courses.name,
-          courses.course_code,
-          courses.settings,
+          c.id,
+          c.name,
+          c.course_code,
+          c.settings,
+          cc.content,
           'learning_path' AS content_type,
-          courses.id AS learning_path_id,
+          c.id AS learning_path_id,
           0 AS learning_objective_id
-        FROM courses
+        FROM courses c
           LEFT OUTER JOIN fearless_taggings ts
-            ON ts.taggable_id = courses.id AND ts.taggable_type = 'LearningPath'
+            ON ts.taggable_id = c.id AND ts.taggable_type = 'LearningPath'
           LEFT OUTER JOIN fearless_tags t
             ON t.id = ts.tag_id
+          LEFT OUTER JOIN fearless_custom_contents cc
+            ON cc.contentable_id = c.id AND cc.contentable_type = 'LearningPath'
         WHERE 0=0
           #{construct_account_clause}
           #{construct_course_worklow_clause}
@@ -49,48 +52,54 @@ module Queries
         UNION ALL
         -- Search learning objectives
         SELECT DISTINCT
-          context_modules.id,
-          context_modules.name,
-          courses.course_code,
-          courses.settings,
+          cm.id,
+          cm.name,
+          c.course_code,
+          c.settings,
+          cc.content,
           'learning_objective' AS content_type,
-          context_modules.context_id::bigint AS learning_path_id,
-          context_modules.id::bigint AS learning_objective_id
-        FROM context_modules
-          INNER JOIN courses
-            ON courses.id = context_modules.context_id
-              AND context_modules.context_type = 'Course'
+          cm.context_id::bigint AS learning_path_id,
+          cm.id::bigint AS learning_objective_id
+        FROM context_modules cm
+          INNER JOIN courses c
+            ON c.id = cm.context_id
+              AND cm.context_type = 'Course'
           LEFT OUTER JOIN fearless_taggings ts
-            ON ts.taggable_id = context_modules.id AND ts.taggable_type = 'LearningObjective'
+            ON ts.taggable_id = cm.id AND ts.taggable_type = 'LearningObjective'
           LEFT OUTER JOIN fearless_tags t
             ON t.id = ts.tag_id
+          LEFT OUTER JOIN fearless_custom_contents cc
+            ON cc.contentable_id = cm.id AND cc.contentable_type = 'LearningObjective'
         WHERE 0=0
           #{construct_account_clause}
-          #{construct_generic_workflow_clause('context_modules')}
-          #{construct_name_sql('context_modules')}
+          #{construct_generic_workflow_clause('cm')}
+          #{construct_name_sql('cm')}
           #{construct_all_tags_serach('t', 'name')}
         UNION ALL
         -- Search learning learning_event
         SELECT DISTINCT
-          content_tags.id,
-          content_tags.title AS name,
-          courses.course_code,
-          courses.settings,
+          ct.id,
+          ct.title AS name,
+          c.course_code,
+          c.settings,
+          cc.content,
           'learning_event' AS content_type,
-          content_tags.context_id::bigint AS learning_path_id,
-          content_tags.context_module_id::bigint AS learning_objective_id
-        FROM content_tags
-          INNER JOIN courses
-            ON courses.id = content_tags.context_id
-              AND content_tags.context_type = 'Course'
+          ct.context_id::bigint AS learning_path_id,
+          ct.context_module_id::bigint AS learning_objective_id
+        FROM content_tags ct
+          INNER JOIN courses c
+            ON c.id = ct.context_id
+              AND ct.context_type = 'Course'
           LEFT OUTER JOIN fearless_taggings ts
-            ON ts.taggable_id = content_tags.id AND ts.taggable_type = 'LearningEvent'
+            ON ts.taggable_id = ct.id AND ts.taggable_type = 'LearningEvent'
           LEFT OUTER JOIN fearless_tags t
             ON t.id = ts.tag_id
+          LEFT OUTER JOIN fearless_custom_contents cc
+            ON cc.contentable_id = ct.id AND cc.contentable_type = 'LearningEvent'
         WHERE 0=0
           #{construct_account_clause}
-          #{construct_generic_workflow_clause('content_tags')}
-          #{construct_name_sql('content_tags', 'title')}
+          #{construct_generic_workflow_clause('ct')}
+          #{construct_name_sql('ct', 'title')}
           #{construct_all_tags_serach('t', 'name')}
       SQL
     end
@@ -103,15 +112,15 @@ module Queries
       "AND " + sql_strings.join(" AND ") unless sql_strings.empty?
     end
 
-    def construct_account_clause(table = "courses")
+    def construct_account_clause(table = "c")
       "AND #{table}.account_id = #{sanitize(account_id)}" if account_id.present?
     end
 
     def construct_course_worklow_clause
       if workflow_state == "active"
-        "AND courses.workflow_state = 'available'"
+        "AND c.workflow_state = 'available'"
       else
-        "AND courses.workflow_state != 'available'"
+        "AND c.workflow_state != 'available'"
       end
     end
 
@@ -127,7 +136,7 @@ module Queries
       "#{concat} #{table}.#{field} ILIKE '#{sanitize(value)}'" if value.present?
     end
 
-    def construct_name_sql(table = "courses", field = "name")
+    def construct_name_sql(table = "c", field = "name")
       construct_ilike_search(table, field, keywords)
     end
 
