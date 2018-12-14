ff0d767b6dde2e23133f7b1d127dee2e0166b6df
Nicholas Watson <nicholas.w.watson@me.com>
2018-12-14 07:08:32 -0500

    added documentation to application_controller.rb

--- service-layer/app/controllers/application_controller.rb
+++ service-layer/app/controllers/application_controller.rb
@@ -6,6 +6,16 @@ class ApplicationController < ActionController::Base
     render json: resource.to_json, status: status
   end
 
+  # This method will return validation errors that occur when creating or
+  # updating a record. Below is an example of its use:
+  #
+  #  def update
+  #    if contentable.update(model_params)
+  #      render json: return_json(contentable), status: :ok
+  #    else
+  #      render errors_for(content)
+  #    end
+  #  end
   def errors_for(object)
     { json: { errors: object.errors }, status: :unprocessable_entity }
   end
