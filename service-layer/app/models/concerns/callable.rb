f2a550eafb61f40eb80ab657ec4c020dcd2dcfb7
Nicholas Watson <nicholas.w.watson@me.com>
2018-12-14 07:09:22 -0500

    added documentation to callable.rb

--- service-layer/app/models/concerns/callable.rb
+++ service-layer/app/models/concerns/callable.rb
@@ -1,3 +1,24 @@
+# The Callable Concern provides a way to quickly instantiate and call the call
+# method on service, command or other types of objects. Here is an example of
+# its use:
+#
+#   class HelloWorld
+#     include Callable
+#
+#     def initialize(name)
+#       @name = name
+#     end
+#
+#     def call
+#       "Hello #{name}!"
+#     end
+#   end
+#
+#   HelloWorld.call("Nick")
+#
+# This method call would return
+#
+#   Hello Nick!
 module Callable
   extend ActiveSupport::Concern
 
